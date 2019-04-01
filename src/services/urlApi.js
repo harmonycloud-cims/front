795fdadcb47a305917b003f6d8bb754f62b2dceb
杜静 <jing@dujingdeMacBook-Pro.local>
2019-04-01 09:26:14 +0800

    location.host

--- src/services/urlApi.js
+++ src/services/urlApi.js
@@ -1,3 +1,3 @@
-export const socketApiUrl = '58.16.78.136:33000';
+// export const socketApiUrl = '58.16.78.136:33000';
 // export const socketApiUrl = '10.10.103.61:33010';
-// export const socketApiUrl = window.location.host;
+export const socketApiUrl = window.location.host;
